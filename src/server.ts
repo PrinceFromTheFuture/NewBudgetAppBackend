bb826dee4cebac669ba07adcf9a2a02b766e853b
Amir <amirwais.projects@gmail.com>
2024-05-11 13:06:06 +0300

    mannn

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,17 @@ const app = express();
 
 app.use(cors({ credentials: true }));
 
+//Enabling CORS
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 configDotenv();
 app.use(cookieParser());
