4b7a1acf6d9149c81d13d4f6c4b7244f662bc777
Amir <amirwais.projects@gmail.com>
2024-05-11 13:43:24 +0300

    mannn

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import { sourceInterface } from "./types.js";
@@ -7,23 +8,18 @@ import cookieParser from "cookie-parser";
 import transactionsRouter from "./routes/transactionsRouter/transactionsRouter.js";
 import budgetsRouter from "./routes/budgetsRouter/budgetsRouter.js";
 import authRouter from "./authRouter.js";
+configDotenv();
 
 const app = express();
 
-//Enabling CORS
+const frontendIp = process.env.FRONTEND;
+const corsOptions = {
+  origin: frontendIp,
+  credentials: true, // Allow credentials
+};
+app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND}`); // Change this to match your frontend origin
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 app.use(express.urlencoded({ extended: false }));
-configDotenv();
 app.use(cookieParser());
 app.use(express.json());
 
